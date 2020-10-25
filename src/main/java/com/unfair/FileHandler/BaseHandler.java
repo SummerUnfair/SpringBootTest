93184ff39abaea4978be11667897fbb2b362878f
SummerUnfair <928971634@qq.com>
2020-10-25 17:43:59 +0800

    2020/2/26 springboot格式及整合mybatis

--- src/main/java/com/unfair/FileHandler/BaseHandler.java
+++ src/main/java/com/unfair/FileHandler/BaseHandler.java
@@ -12,9 +12,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
 
 /**
  * @author fenghao
@@ -32,7 +34,8 @@ public class BaseHandler {
 
         BaseHandler baseHandler = new BaseHandler();
         baseHandler.initData();
-        ModelEnum modelEnum = ModelEnum.TO_TXT;
+        ModelEnum modelEnum = ModelEnum.TO_DATABASE;
+        logger.info("执行任务当前日期:{}", baseHandler.getThisToday());
         switch (modelEnum) {
             case TO_TXT:
                 ReturnT returnT = baseHandler.readToTxt(inPath, null);
@@ -42,6 +45,12 @@ public class BaseHandler {
                 baseHandler.readToDataBase(null);
                 break;
         }
+    }
+
+    @Test
+    public void UseTest() {
+
+        System.out.println(File.separator);
     }
 
     public void initData() {
@@ -59,7 +68,7 @@ public class BaseHandler {
      */
     public ReturnT readToTxt(String in, String out) {
 
-        if (StringUtils.isEmpty(in)){
+        if (StringUtils.isEmpty(in)) {
             logger.info("读取路径不能为null");
             return ReturnT.builder()
                     .message("读取路径不能为null")
@@ -67,7 +76,7 @@ public class BaseHandler {
                     .build();
         }
 
-        if (!StringUtils.endsWith(in,".txt")){
+        if (!StringUtils.endsWith(in, ".txt")) {
             logger.info("文件格式不正确");
             return ReturnT.builder()
                     .message("文件格式不正确")
@@ -75,7 +84,22 @@ public class BaseHandler {
                     .build();
         }
 
+        String baseStrPath = in.substring(0, in.lastIndexOf(File.separator));
+        File basePath = new File(baseStrPath);
+        if (!basePath.exists()) {
+            logger.info("当前路径不存在:{}", baseStrPath);
+            return ReturnT.builder()
+                    .message("当前路径不存在:" + baseStrPath)
+                    .returnData(in)
+                    .build();
+        }
         logger.info("读取文件：{} | 写入文件：{}", in, out);
+
+        File[] listFile = basePath.listFiles();
+        List<File> files = Arrays.asList(listFile);
+        long count = files.stream().filter(file -> StringUtils.endsWith(file.toString(), ".txt")).count();
+        logger.info("同级文本数(txt)：{} ", count);
+
         long startTime = 0;
         long endTime = 0;
         long useTime = 0;
@@ -86,7 +110,7 @@ public class BaseHandler {
             if (!file.exists()) {
                 logger.info("当前路径下未找到文件:{}", in);
                 return ReturnT.builder()
-                        .message("未找到文件:"+in)
+                        .message("未找到文件:" + in)
                         .returnData(in)
                         .build();
             }
@@ -111,12 +135,12 @@ public class BaseHandler {
                 logger.info("BufferedReader关闭异常");
                 e.printStackTrace();
             }
-            if (startTime !=0){
+            if (startTime != 0) {
                 useTime = endTime - startTime;
                 logger.info("任务耗时:{}毫秒", useTime);
                 return ReturnT.builder()
                         .message("SUCCESS")
-                        .returnData("任务耗时:{}毫秒"+useTime)
+                        .returnData("任务耗时:{}毫秒" + useTime)
                         .build();
             }
             return ReturnT.builder()
@@ -127,12 +151,48 @@ public class BaseHandler {
     }
 
     public void readToDataBase(String in) {
-//        Connection conn = getConnection();
-//        PreparedStatement ps = conn.prepareStatement("select *from usr where name = ？");
-//        ps.setString(1, "丽丽");
-//        ps.executeUpdate();
-//        conn.commit();
+        Connection conn = getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement("select *from usr where username = ?");
+            ps.setString(1, "丽丽");
+            rs = ps.executeQuery();
 
+            while (rs.next()) {
+                String id = rs.getString("id");
+                String username = rs.getString("username");
+                System.out.println(id + ":" + username);
+            }
+            conn.commit();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (ps != null) {
+                        ps.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (conn != null) {
+                            conn.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 
     public Connection getConnection() {
@@ -156,6 +216,20 @@ public class BaseHandler {
         }
     }
 
+    public String getThisToday() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        String start = sdf.format(cal.getTime());
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
+        String end = sdf.format(cal.getTime());
+        return start + "|" + end;
+    }
+
     public enum ModelEnum {
 
         TO_TXT, TO_DATABASE;
@@ -168,6 +242,7 @@ public class BaseHandler {
         public ReadToSpecificException() {
             super();
         }
+
         public ReadToSpecificException(String message, Throwable cause) {
             super(message, cause);
         }
@@ -175,6 +250,7 @@ public class BaseHandler {
         public ReadToSpecificException(String message) {
             super(message);
         }
+
         public ReadToSpecificException(Throwable cause) {
             super(cause);
         }
@@ -195,10 +271,11 @@ public class BaseHandler {
     }
 
 }
+
 @Data
 @AllArgsConstructor
 @Builder
-class ReturnT<T>{
+class ReturnT<T> {
     private T returnData;
     private String message;
 }
